f436de71c4dddbfdf2547c0fc00e371b54301584
MattYu12138 <164812398+MattYu12138@users.noreply.github.com>
2025-07-21 18:51:37 +1000

    增加hello测试接口，使用http

--- wiki/src/main/java/com/Matt/wiki/controller/TestController.java
+++ wiki/src/main/java/com/Matt/wiki/controller/TestController.java
@@ -1,8 +1,7 @@
 package com.Matt.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +10,10 @@ public class TestController {
     public String hello(){
         return "hello world";
     }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "hello world! Post, " + name + ".";
+    }
+
 }
