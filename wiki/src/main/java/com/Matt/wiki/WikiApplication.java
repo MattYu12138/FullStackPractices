55b21a9955a6880a154f090b36a73006842a8f3b
MattYu12138 <164812398+MattYu12138@users.noreply.github.com>
2025-07-21 13:10:27 +1000

    更改控制台输出格式

--- wiki/src/main/java/com/Matt/wiki/WikiApplication.java
+++ wiki/src/main/java/com/Matt/wiki/WikiApplication.java
@@ -1,13 +1,23 @@
 package com.Matt.wiki;
 
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikiApplication {
+	private static final Log LOG = LogFactory.getLog(WikiApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(WikiApplication.class, args);
+//		SpringApplication.run(WikiApplication.class, args);
+		SpringApplication app = new SpringApplication(WikiApplication.class);
+
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("Start Successful");
+		LOG.info("地址:\thttp://127.0.0.1:" + env.getProperty("server.port"));
+
 	}
 
 }
