b86ed98f5c870c8bc2439b7576987d5f7838e34f
MattYu12138 <164812398+MattYu12138@users.noreply.github.com>
2025-07-22 18:12:41 +1000

    集成mybatis

--- wiki/src/main/java/com/Matt/wiki/controller/TestController.java
+++ wiki/src/main/java/com/Matt/wiki/controller/TestController.java
@@ -5,21 +5,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 public class TestController {
 
     @Value("${hello.test}")
-    private String testHello;
+    private String testH;
 
 
     @GetMapping("/hello")
     public String hello(){
-        return "hello world" + testHello;
+        return "hello world" + testH;
     }
+
+
 
     @PostMapping("/hello/post")
     public String helloPost(String name){
         return "hello world! Post, " + name + ".";
     }
+
 
 }
