c9888eb7f3b43fdebbb80847039d7dc70da3189b
MattYu12138 <164812398+MattYu12138@users.noreply.github.com>
2025-07-23 05:50:43 +1000

    制作the-footer组件,并删除helloworld.vue

---
+++ wiki/web/src/components/the-footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Hello Hello Hello Hello Hello2222324
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer'
+});
+</script>
