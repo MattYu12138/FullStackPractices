a976823677a741e72690958acbcafe00ca8e7c5b
MattYu12138 <164812398+MattYu12138@users.noreply.github.com>
2025-07-22 21:59:58 +1000

    集成mybatis generator，生成demo表代码

--- wiki/doc/db/all.sql
+++ wiki/doc/db/all.sql
@@ -4,10 +4,18 @@ create table `test` (
   `name` varchar(50) comment 'name',
   `password` varchar(50) comment 'password',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 insert into `test` (id, name, password) values (2, 'test', 'ysz991203');
 
-SHOW GRANTS FOR 'wiki'@'%';
 
+DROP TABLE IF EXISTS `demo`;
+-- 创建优化后的表
+CREATE TABLE `demo` (
+                        `id` BIGINT NOT NULL COMMENT 'Unique identifier for the record',
+                        `name` VARCHAR(50) NOT NULL COMMENT 'Name for the entity',
+                        PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Table to store demo information';
 
+-- 插入数据测试
+INSERT INTO `demo` (id, name) VALUES (1, 'test');
