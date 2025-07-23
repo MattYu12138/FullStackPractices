ad7da34c162d8a6aa64fb8df547060b533bd2b87
MattYu12138 <164812398+MattYu12138@users.noreply.github.com>
2025-07-23 20:16:45 +1000

    增加aop请求和返回，去掉过滤器，拦截器

--- wiki/src/main/java/com/matt/wiki/config/SpringMvcConfig.java
+++ wiki/src/main/java/com/matt/wiki/config/SpringMvcConfig.java
@@ -1,32 +1,29 @@
 package com.matt.wiki.config;
 
-import com.matt.wiki.interceptor.LoginInterceptor;
-import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
-    @Resource
-    LoginInterceptor loginInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns(
-                        "/test/**",
-                        "/redis/**",
-                        "/user/login",
-                        "/category/all",
-                        "/ebook/list",
-                        "/doc/all/**",
-                        "/doc/vote/**",
-                        "/doc/find-content/**",
-                        "/ebook-snapshot/**",
-                        "/ebook/upload/avatar",
-                        "/file/**"
-                );
-    }
+//    @Resource
+//    LoginInterceptor loginInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(loginInterceptor)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns(
+//                        "/test/**",
+//                        "/redis/**",
+//                        "/user/login",
+//                        "/category/all",
+//                        "/ebook/list",
+//                        "/doc/all/**",
+//                        "/doc/vote/**",
+//                        "/doc/find-content/**",
+//                        "/ebook-snapshot/**",
+//                        "/ebook/upload/avatar",
+//                        "/file/**"
+//                );
+//    }
 }
