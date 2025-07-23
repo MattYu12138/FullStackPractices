4aacdea0798a864d20fa8b36676bdb933928029b
MattYu12138 <164812398+MattYu12138@users.noreply.github.com>
2025-07-23 21:15:39 +1000

    增加管理界面， 新页面开发包括：页面、路由、菜单

--- wiki/web/src/components/the-header.vue
+++ wiki/web/src/components/the-header.vue
@@ -1,20 +1,26 @@
 <template>
   <a-layout-header class="header">
-    <div class="logo" />
+    <div class="logo"/>
     <a-menu
         theme="dark"
         mode="horizontal"
         :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="1">nav 1111</a-menu-item>
-      <a-menu-item key="2">nav 2</a-menu-item>
-      <a-menu-item key="3">nav 3</a-menu-item>
+      <a-menu-item key="/">
+        <router-link to="/">Home Page</router-link>
+      </a-menu-item>
+      <a-menu-item key="/admin/ebook">
+        <router-link to="/admin/ebook">Ebook Manage Page</router-link>
+      </a-menu-item>
+      <a-menu-item key="/about">
+        <router-link to="/about">About Us</router-link>
+      </a-menu-item>
     </a-menu>
   </a-layout-header>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
 
 export default defineComponent({
   name: 'the-header'
