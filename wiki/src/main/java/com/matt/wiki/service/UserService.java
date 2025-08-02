51571a99a41d68100578b162aa49d11cca361fb7
MattYu12138 <164812398+MattYu12138@users.noreply.github.com>
2025-08-03 06:03:01 +1000

    配置header的登陆弹窗，但显示位置不正确。 错误显示报错信息

--- wiki/src/main/java/com/matt/wiki/service/UserService.java
+++ wiki/src/main/java/com/matt/wiki/service/UserService.java
@@ -127,7 +127,7 @@ public class UserService {
         if(ObjectUtils.isEmpty(userDB)){
 //            用户名不存在
             LOG.warn("User not found for login name: " + req.getLoginName());
-            throw new BusinessException(BusinessExceptionCode.USER_LOGIN_NAME_EXIST);
+            throw new BusinessException(BusinessExceptionCode.LOGIN_USER_ERROR);
         }else{
             if(userDB.getPassword().equals(req.getPassword())){
 //                登录成功
@@ -136,7 +136,7 @@ public class UserService {
             }else{
 //                密码不对
                 LOG.warn("密码不对，输入密码：{}, 数据库密码：{}",req.getPassword(), userDB.getPassword());
-                throw new BusinessException(BusinessExceptionCode.USER_LOGIN_NAME_EXIST);
+                throw new BusinessException(BusinessExceptionCode.LOGIN_USER_ERROR);
 
             }
         }
--- wiki/web/src/components/the-header.vue
+++ wiki/web/src/components/the-header.vue
@@ -4,7 +4,7 @@
     <a-menu
         theme="dark"
         mode="horizontal"
-        :style="{ lineHeight: '64px' }"
+        :style="{ lineHeight: '64px' , flex: 1}"
     >
       <a-menu-item key="/">
         <router-link to="/">Home Page</router-link>
@@ -23,13 +23,133 @@
         <router-link to="/about">About Us</router-link>
       </a-menu-item>
     </a-menu>
+    <div class="login-area">
+      <a-popconfirm
+          title="确认退出登录?"
+          ok-text="是"
+          cancel-text="否"
+          @confirm="logout()"
+          v-if="loginConfig.user.id"
+      >
+        <a class="login-menu">
+          <span>退出登录</span>
+        </a>
+      </a-popconfirm>
+
+      <a class="login-menu" v-show="loginConfig.user.id">
+        <span>您好：{{loginConfig.user.name}}</span>
+      </a>
+      <a class="login-menu" v-show="!loginConfig.user.id" @click="showLoginModal">
+        <span>登录</span>
+      </a>
+    </div>
+
+    <a-modal
+        title="登录"
+        v-model:visible="loginConfig.visible"
+        :confirm-loading="loginConfig.loading"
+        @ok="login"
+    >
+      <a-form :model="loginConfig.user" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+        <a-form-item label="登录名">
+          <a-input v-model:value="loginConfig.user.loginName" />
+        </a-form-item>
+        <a-form-item label="密码">
+          <a-input v-model:value="loginConfig.user.password" type="password" />
+        </a-form-item>
+      </a-form>
+    </a-modal>
+
+
   </a-layout-header>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+import {defineComponent, computed, reactive} from 'vue';
+import axios from 'axios';
+import { message } from 'ant-design-vue';
+import store from "@/store";
+
+declare let hexMd5: any;
+declare let KEY: any;
 
 export default defineComponent({
-  name: 'the-header'
+  name: 'the-header',
+
+  setup () {
+    // 登录后保存
+    const user = computed(() => store.state.user);
+
+    // 用来登录
+
+    const loginConfig = reactive({
+      user:{
+        loginName:"test",
+        password:"test"
+      },
+      visible : false,
+      loading : false,
+    })
+
+
+    const showLoginModal = () => {
+      loginConfig.visible= true;
+    };
+
+    // 登录
+    const login = () => {
+      console.log("开始登录");
+      loginConfig.loading = true;
+      loginConfig.user.password = hexMd5(loginConfig.user.password + KEY);
+      axios.post('/user/login', loginConfig.user).then((response) => {
+        loginConfig.loading = false;
+        const data = response.data;
+        if (data.success) {
+          loginConfig.visible = false;
+          message.success("登录成功！");
+
+          store.commit("setUser", data.content);
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    // 退出登录
+    const logout = () => {
+      console.log("退出登录开始");
+      axios.get('/user/logout/' + user.value.token).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          message.success("退出登录成功！");
+          store.commit("setUser", {});
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    return {
+      showLoginModal,
+      loginConfig,
+      login,
+      user,
+      logout
+    }
+  }
+
 });
 </script>
+
+<style>
+.login-area {
+  float: right;
+}
+
+.login-menu {
+  color: white;
+  margin-left: 16px;
+}
+</style>
+
+
