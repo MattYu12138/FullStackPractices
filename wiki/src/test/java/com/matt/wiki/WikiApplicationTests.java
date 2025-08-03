ae717e22e82d35f17f4ca0611ee8986489f0630a
MattYu12138 <164812398+MattYu12138@users.noreply.github.com>
2025-08-04 08:13:22 +1000

    fix: isolate tests from external services

--- wiki/src/test/java/com/matt/wiki/WikiApplicationTests.java
+++ wiki/src/test/java/com/matt/wiki/WikiApplicationTests.java
@@ -2,12 +2,19 @@ package com.matt.wiki;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WikiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockBean
+    private RedisTemplate<?, ?> redisTemplate;
+
+    @Test
+    void contextLoads() {
+    }
 
 }
