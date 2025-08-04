ce23b75b00154e724a54e72730b17ac7aa95f8d1
MattYu12138 <164812398+MattYu12138@users.noreply.github.com>
2025-08-04 11:22:39 +1000

    增加统计数值接口

---
+++ wiki/src/main/java/com/matt/wiki/controller/EbookSnapshotController.java
@@ -0,0 +1,35 @@
+package com.matt.wiki.controller;
+
+import com.matt.wiki.resp.CommonResp;
+import com.matt.wiki.resp.StatisticResp;
+import com.matt.wiki.service.EbookSnapshotService;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook-snapshot")
+public class EbookSnapshotController {
+
+    @Resource
+    private EbookSnapshotService ebookSnapshotService;
+
+    @GetMapping("/get-statistic")
+    public CommonResp getStatistic() {
+        List<StatisticResp> statisticResp = ebookSnapshotService.getStatistic();
+        CommonResp<List<StatisticResp>> commonResp = new CommonResp<>();
+        commonResp.setContent(statisticResp);
+        return commonResp;
+    }
+
+    @GetMapping("/get-30-statistic")
+    public CommonResp get30Statistic() {
+        List<StatisticResp> statisticResp = ebookSnapshotService.get30Statistic();
+        CommonResp<List<StatisticResp>> commonResp = new CommonResp<>();
+        commonResp.setContent(statisticResp);
+        return commonResp;
+    }
+
+}
