dda65e75c225a44a57f4bd89ac352cca82180bdd
MattYu12138 <164812398+MattYu12138@users.noreply.github.com>
2025-08-25 07:47:31 +1000

    修改LLMService,使用spring容器注入依赖

--- wiki/src/test/java/com/matt/wiki/LLMFactoryTest.java
+++ wiki/src/test/java/com/matt/wiki/LLMFactoryTest.java
@@ -3,19 +3,26 @@ package com.matt.wiki;
 import com.matt.wiki.factory.LLMFactory;
 import com.matt.wiki.factory.service.LLMService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+
+@SpringBootTest
 public class LLMFactoryTest {
+
+    @Autowired
+    private LLMFactory llmFactory;
 
     @Test
     public void testOpenAiFactoryReturnsService() {
-        LLMService llmService = LLMFactory.createLLMService(LLMFactory.modelType.OPENAI);
+        LLMService llmService = llmFactory.createLLMService(LLMFactory.modelType.OPENAI);
         String result = llmService.chat("hello");
         System.out.println(result);
     }
 
     @Test
     public void testQwenFactoryWithoutApiKey() {
-        LLMService llmService = LLMFactory.createLLMService(LLMFactory.modelType.QWEN);
+        LLMService llmService = llmFactory.createLLMService(LLMFactory.modelType.QWEN);
         String result = llmService.chat("hello");
         System.out.println(result);
     }
