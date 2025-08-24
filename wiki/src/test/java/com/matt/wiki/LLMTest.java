bed13825077149e207f52a44a3190e77ad544cce
MattYu12138 <164812398+MattYu12138@users.noreply.github.com>
2025-08-25 07:08:02 +1000

    配置LLM的key，以及LLMTest类

--- wiki/src/test/java/com/matt/wiki/LLMTest.java
+++ wiki/src/test/java/com/matt/wiki/LLMTest.java
@@ -1,24 +1,54 @@
 package com.matt.wiki;
 
 
-import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.community.model.dashscope.WanxImageModel;
+import dev.langchain4j.data.image.Image;
+import dev.langchain4j.model.output.Response;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 public class LLMTest {
 
-    @Test
-    public void helloWorld(){
-        OpenAiChatModel model = OpenAiChatModel.builder()
-                .baseUrl("http://langchain4j.dev/demo/openai/v1")
-                .apiKey("demo")
-                .modelName("gpt-4o-mini")
-                .build();
+    @MockBean
+    private RedisTemplate redisTemplate;
+//
+//    @Autowired
+//    private OpenAiChatModel openAiChatModel;
+//
+//    @Test
+//    public void testDeepSeekViaOpenAiCompatibleApi() {
+//        String answer = openAiChatModel.chat("你是谁？");
+//        System.out.println(answer);
+//    }
 
-        String answer = model.chat("Hello, how are you?");
-        System.out.println(answer);
+
+
+
+    @Autowired
+    private QwenChatModel qwenChatModel;
+
+    @Test
+    public void testDashScope(){
+        String chat = qwenChatModel.chat("你是谁？");
+        System.out.println(chat);
+    }
+
+
+    @Test
+    public void testDashScopeWanx(){
+        WanxImageModel wanxImageModel = WanxImageModel.builder()
+                .modelName("wanx2.1-t2i-plus")
+                .apiKey("sk-86c0a5ecbf284c0b9b58da54a394fcf2")
+                .build();
+        Response<Image> response = wanxImageModel.generate("美女");
+        System.out.println(response.content().url());
+
     }
 }
