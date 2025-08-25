5812ac502032cf4327f178129513091d46470be3
MattYu12138 <164812398+MattYu12138@users.noreply.github.com>
2025-08-26 07:56:09 +1000

    解决没有生成bean的问题，

--- wiki/src/main/java/com/matt/wiki/aiservice/AiAssistant.java
+++ wiki/src/main/java/com/matt/wiki/aiservice/AiAssistant.java
@@ -7,8 +7,8 @@ import dev.langchain4j.service.spring.AiService;
 import dev.langchain4j.service.spring.AiServiceWiringMode;
 import reactor.core.publisher.Flux;
 
-@AiService(wiringMode = AiServiceWiringMode.EXPLICIT, chatModel = "QwenChatModel",
-        streamingChatModel = "QwenStreamingChatModel", chatMemoryProvider = "chatMemoryProvider", tools = "testUtil")
+@AiService(wiringMode = AiServiceWiringMode.EXPLICIT, chatModel = "qwenChatModel",
+        streamingChatModel = "qwenStreamingChatModel", chatMemoryProvider = "chatMemoryProvider", tools = "testUtil")
 public interface AiAssistant {
 
     String chat(@MemoryId  String id, @UserMessage String message);
