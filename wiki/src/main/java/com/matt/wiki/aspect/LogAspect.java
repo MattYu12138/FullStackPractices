a6940a2391cea4abcef21f38c5d307cf8dbca3d7
MattYu12138 <164812398+MattYu12138@users.noreply.github.com>
2025-08-26 08:22:55 +1000

    Avoid serializing reactive types in logging

--- wiki/src/main/java/com/matt/wiki/aspect/LogAspect.java
+++ wiki/src/main/java/com/matt/wiki/aspect/LogAspect.java
@@ -22,6 +22,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
+import org.reactivestreams.Publisher;
 
 @Aspect
 @Component
@@ -58,16 +59,20 @@ public class LogAspect {
 
         // 打印请求参数
         Object[] args = joinPoint.getArgs();
-		// LOG.info("请求参数: {}", JSONObject.toJSONString(args));
+        // LOG.info("请求参数: {}", JSONObject.toJSONString(args));
 
-		Object[] arguments  = new Object[args.length];
+        Object[] arguments  = new Object[args.length];
         for (int i = 0; i < args.length; i++) {
             if (args[i] instanceof ServletRequest
                     || args[i] instanceof ServletResponse
                     || args[i] instanceof MultipartFile) {
                 continue;
             }
-            arguments[i] = args[i];
+            if (args[i] instanceof Publisher) {
+                arguments[i] = args[i].getClass().getSimpleName();
+            } else {
+                arguments[i] = args[i];
+            }
         }
         // 排除字段，敏感字段或太长的字段不显示
         String[] excludeProperties = {"password", "file"};
@@ -87,7 +92,11 @@ public class LogAspect {
         PropertyPreFilters filters = new PropertyPreFilters();
         PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
         excludefilter.addExcludes(excludeProperties);
-        LOG.info("返回结果: {}", JSONObject.toJSONString(result, excludefilter));
+        if (result instanceof Publisher) {
+            LOG.info("返回结果: {}", result.getClass().getSimpleName());
+        } else {
+            LOG.info("返回结果: {}", JSONObject.toJSONString(result, excludefilter));
+        }
         LOG.info("------------- 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
         return result;
     }
